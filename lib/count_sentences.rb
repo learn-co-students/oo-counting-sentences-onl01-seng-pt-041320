373c87c48570d94891556a04457a83f949cc1fe8
Daisuke Hashimoto <daisukehashimoto39@gmail.com>
2020-04-26 06:21:11 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,45 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    array = self.split(/[.?!]/).reject{|string| string.empty?}
+    binding.pry
+    array.count
+    
+    # array = []
+    # array = self.split() 
+    # count = 0
+    
+    # array.each do |word|
+    #   if word.end_with?(".") == true || word.end_with?("?") == true || word.end_with?("!") == true
+    #     count += 1
+    #     #binding.pry
+    #   end
+    # end
+    
+    # count
   end
 end

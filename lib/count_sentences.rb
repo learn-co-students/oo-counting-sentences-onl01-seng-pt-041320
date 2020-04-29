bfcdd37463d1d34b1b41d330dc94fc9b0175f1b0
vedransehovic <vedran.sehovic@gmail.com>
2020-04-29 14:26:24 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    result = []
+    result = self.split(/[?!.]/)
+    result.reject! { |element| element.to_s.empty? }
+    result.length
   end
 end

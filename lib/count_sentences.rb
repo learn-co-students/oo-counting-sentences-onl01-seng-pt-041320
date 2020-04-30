8d42c6079a5d3e401bf2313f0cc4045bd79e1a78
Dwight Nodolf <dnodolf@gmail.com>
2020-04-30 21:39:20 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
+    self.end_with?(".")
     
   end
 
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
+    self.end_with?("!")
 
   end
 
   def count_sentences
-
+    new_string = self.squeeze(".")
+    new_string = self.squeeze("!")
+    new_string.split(/[$.|?|!]/).count 
   end
+    
+    
 end

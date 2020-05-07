52242340aa4182cadade00ba301400bf06fe8508
Ashlen Mencia <ashmencia0103@gmail.com>
2020-05-07 04:31:45 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,31 @@ require 'pry'
 class String
 
   def sentence?
-    
+      if self.end_with?(".")
+        return true
+      end
+        return false
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true
+    end
+      return false
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true
+    end
+      return false
   end
 
   def count_sentences
-
+    # call on count_sentences method and get back a string that has the count
+    # x = 0
+    # count_sentences.string.slice!(x) { |sentence| sentence == x}
+   self.split(/[.?!]+/).count
   end
+
 end
